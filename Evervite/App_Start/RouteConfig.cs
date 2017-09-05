cc71ee47edd72b91f46f6b1c66cd93f08dab735c
pxhpjx <pxhpjx@gmail.com>
2017-09-06 00:02:48 +0800

    路由调整

--- Evervite/App_Start/RouteConfig.cs
+++ Evervite/App_Start/RouteConfig.cs
@@ -12,12 +12,12 @@ namespace Evervite
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("MyRoute", "{controller}/{action}");
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ Evervite/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Evervite.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /ProductList/
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        public ActionResult List(int id)
+        {
+            ViewBag.CCC = id;
+            return View();
+        }
+
+        public ActionResult Detail(string productType)
+        {
+            ViewBag.CCC = productType;
+            return View();
+        }
+
+    }
+}
